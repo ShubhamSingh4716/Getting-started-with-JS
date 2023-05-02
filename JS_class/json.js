f81d7c47ba681f411bbefb50623b6f81a526bf3f
SHUBHAM SINGH <106438656+ShubhamSingh4716@users.noreply.github.com>
2023-05-02 18:33:07 +0530

    Add files via upload

---
+++ JS_class/json.js
@@ -0,0 +1,22 @@
+// document.write("Java" +"<br>")
+// document.write("JavaScript" +"<br>")
+// document.write("SQL" +"<br>")
+// document.write("C++" +"<br>")
+// document.write("C#" +"<br>" + "<br>") 
+
+function display()
+{
+  document.write("Java" +"<br>")
+  document.write("JavaScript" +"<br>")
+  document.write("SQL" +"<br>")
+  document.write("C++" +"<br>")
+  document.write("C#" +"<br>" )
+  document.write("<br>")
+    
+}
+ display()
+ display()
+ display()
+ display()
+ 
+
